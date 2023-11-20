04a7b7a4d68fa041a9e6ae11b5afc877834a08ed
OskarUnleashed <oskar.gyllenor@gmail.com>
2023-11-20 21:20:54 +0100

    Final draft of G

--- src/CharCounter2000/CharacterCounter.java
+++ src/CharCounter2000/CharacterCounter.java
@@ -1,12 +1,13 @@
 package CharCounter2000;
 
+//här så räknas characters och rows
 public class CharacterCounter {
     private int totalCharactersBeforeStop = 0;
     private int rowCount = 0;
-
+    //metoden för att behandla strings
     public boolean processInput(String inputString) {
         boolean stopFound = inputString.toLowerCase().contains("stop");
-
+        //Kalkylerar värderna efter stop
         if (stopFound) {
             int stopIndex = inputString.toLowerCase().indexOf("stop");
             int charactersBeforeStop = stopIndex != -1 ? stopIndex : inputString.length();
@@ -21,12 +22,12 @@ public class CharacterCounter {
 
         return stopFound;
     }
-
+    //metoden för att få det fylla resultatet
     public void displayResult() {
         System.out.println("Total characters before 'stop': " + totalCharactersBeforeStop);
         System.out.println("Total rows before 'stop': " + rowCount);
     }
-
+    //getter metod för att få karaktärerna för test
     public int getTotalCharactersBeforeStop() {
         return totalCharactersBeforeStop;
     }
--- src/CharCounter2000/UserInputReader.java
+++ src/CharCounter2000/UserInputReader.java
@@ -3,20 +3,21 @@ package CharCounter2000;
 import CharCounter2000.CharacterCounter;
 
 import java.util.Scanner;
-
+//Denna del läser av input och 'interacts' med CharacterCounter
 public class UserInputReader {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //flaggan om stop har hittats
         boolean stopFound = false;
 
         CharacterCounter characterCounter = new CharacterCounter();
-
+        //fortsätter läsa av tills 'stop' har upptäckts
         while (!stopFound) {
-            System.out.print("Enter a string (type 'stop' to exit): ");
+            System.out.print("Välkommen till CharacterCount200 (skriv 'stop' för att mata ut): ");
             String inputString = scanner.nextLine();
 
             stopFound = characterCounter.processInput(inputString);
-
+            //Här så printas resultatet ut
             if (stopFound) {
                 characterCounter.displayResult();
             }
